4114bfba00ab48a73cbc88a3137a367fc7367b6f
michel.beyrouty@washmen.com <Michel El Beyrouty>
2020-08-12 09:44:49 +0300

    [core] Add searchParam component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import SearchParams from "./searchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt me!"),
-  //   React.createElement(Pet, {
-  //     name: "Ghost",
-  //     breed: "Loulou",
-  //     animal: "Dog",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Winston",
-  //     breed: "wofh3f",
-  //     animal: "cat",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Whity",
-  //     breed: "cart",
-  //     animal: "cat",
-  //   }),
-  // ]);
 
   return (
     <div>
       <h1 id="someting-important">Adopt me</h1>
-      <Pet name="Ghost" animal="dog" breed="loulou" />
-      <Pet name="Shadow" animal="bird" breed="hawk" />
-      <Pet name="Winston" animal="cat" breed="mixed" />
+      <SearchParams/>
     </div>
   );
 };
